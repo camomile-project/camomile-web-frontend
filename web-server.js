44f904ac53152ef64cc26ce333cb0bec1832af5f
Pierrick <bruneau@lippmann.lu>
2013-10-11 08:18:24 +0200

    Changed port of web server

--- web-server.js
+++ web-server.js
@@ -1,6 +1,6 @@
 var express = require("express"),
 	app     = express(),
-	port    = parseInt(process.env.PORT, 10) || 8080;
+	port    = parseInt(process.env.PORT, 10) || 8070;
 
 app.configure(function(){
 	app.use(express.methodOverride());
