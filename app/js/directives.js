050a9f67825988431da172d356b11070a1c489fd
Pierrick <bruneau@lippmann.lu>
2013-10-13 13:36:24 +0200

    Before starting TL dev

--- app/js/directives.js
+++ app/js/directives.js
@@ -62,8 +62,6 @@ angular.module('myApp.directives', ['ui.utils']).
 		return {
 			restrict: 'C',
 			link: function(scope, element, attrs) {
-				//var clickElmt = angular.element(element.children()[0]);
-				//var clickElmt = angular.element(element.children()[0]);
 				scope.$watch('model.selected', function(newValue, oldValue, scope) {
 					if(element.hasClass('alert') && (newValue !== scope.$index)) {
 						element.removeClass('round').removeClass('alert');
@@ -90,4 +88,30 @@ angular.module('myApp.directives', ['ui.utils']).
 			}
 		}
 	});
+//	.directive('cmTimeline', function() {
+//		var initComp = function(scope, element) {
+//			// init dimensions/margins
+//
+//			// use time scale, ordinal scale for band layout,
+//			// and color scale for lane design (background with alpha value)
+//
+//			// compute luminance of colors to determine color of overlayed font
+//
+//			// derive utility methods to extract channels from RGB string
+//			// see eg http://www.javascripter.net/faq/octalsan.htm
+//		};
+//		var updateComp = function(scope, element) {
+//		};
+//		return {
+//			restrict: 'E',
+//			replace: true,
+//			template: '<svg></svg>',
+//			link: function(scope, element, attrs) {
+//				initComp();
+//				scope.$watch('model.selected', function(newValue, oldValue, scope) {
+//					updateComp();
+//				});
+//			}
+//		}
+//	});
 
