d77a1a01d0cfa94b558cd84552ca4b6c345bd89a
Pierrick <bruneau@lippmann.lu>
2013-10-18 12:40:56 +0200

    Unstable

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers'])
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers', 'ngRoute'])
 	.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {templateUrl: 'partials/inspect.html'}); // no need for controller property
 																																			// - no need for root controller
