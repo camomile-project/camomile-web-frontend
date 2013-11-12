500bf9d74643f1f8c9d912c19b7805532d475511
Pierrick Bruneau <bruneau@lippmann.lu>
2013-11-12 16:32:31 +0100

    Stable - currently implem. computed layers support

--- app/js/directives.js
+++ app/js/directives.js
@@ -170,7 +170,15 @@ angular.module('myApp.directives', ['myApp.filters']).
 					var brushRange = brush.extent().map(x2MsScale);
 					xMsScale.domain(brush.empty() ? x2MsScale.domain() : brush.extent());
 					xTimeScale.domain(brush.empty() ? x2TimeScale.domain() : brushRange.map(x2TimeScale.invert));
-					refresh();
+
+					focus.selectAll(".annot")
+						.attr("x", function(d) {
+							return xMsScale(d.fragment.start);
+						})
+						.attr("width", function(d) {
+							return xMsScale(d.fragment.end)-xMsScale(d.fragment.start);
+						});
+
 					focus.select(".x.axis").call(xAxis);
 				};
 
@@ -242,44 +250,59 @@ angular.module('myApp.directives', ['myApp.filters']).
 
 
 
-				var addLayer = function(layer) {
-					// define default mapping if needed
-					if(layer.mapping === undefined) {
-						layer.mapping = {
+				var updateLayers = function(addedLayerId, removedLayerId) {
+					var addedLayer, removedLayer;
+					if(addedLayerId !== undefined) {
+						addedLayer = scope.model.availableLayers.filter(function(l) {
+							return(l._id === addedLayerId);
+						})[0];
+					}
+					if(removedLayerId !== undefined) {
+						removedLayer = scope.model.availableLayers.filter(function(l) {
+							return(l._id === removedLayerId);
+						})[0];
+					}
+
+					// update color scale consistently if layer has been added
+					if((addedLayer !== undefined) && (addedLayer.mapping === undefined)) {
+						addedLayer.mapping = {
 							getKey: function(d) {
 								return d.data;
 							}
 						};
 					}
 
-					if(layer.tooltipFunc === undefined) {
-						layer.tooltipFunc = function(d) {
+					if((addedLayer !== undefined) && (addedLayer.tooltipFunc === undefined)) {
+						addedLayer.tooltipFunc = function(d) {
 							return d.data;
 						};
 					}
 
-					if(layer.mapping.colors === undefined){
-						// use default mapping, or define a custom one
-						var vals = layer.layer.map(layer.mapping.getKey);
-						vals = $.grep(vals, function(v, k){
-							return $.inArray(v ,vals) === k;
-						}); // jQuery hack to get Array of unique values
-						// and then all that are not already in the scale domain
-						vals = vals.filter(function(l) {return !(colScale.domain().indexOf(l) > -1);});
-						vals.forEach(function(d) {
-							colScale.domain().push(d);
-							colScale.domain(colScale.domain()); // hack to register changes
-							colScale.range().push(refColors[curColInd]);
-							colScale.range(colScale.range());
-							curColInd = (curColInd + 1) % refColors.length;
-						});
-					} else {
-						for(var key in layer.mapping.colors) {
-							colScale.domain().push(d);
-							colScale.domain(colScale.domain());
-							colScale.range().push(layer.mapping.colors[key]);
-							colScale.range(colScale.range());
+					if(addedLayer !== undefined) {
+						if(addedLayer.mapping.colors === undefined) {
+							// use default mapping, or define a custom one
+							var vals = addedLayer.layer.map(addedLayer.mapping.getKey);
+							vals = $.grep(vals, function(v, k){
+								return $.inArray(v ,vals) === k;
+							}); // jQuery hack to get Array of unique values
+							// and then all that are not already in the scale domain
+							vals = vals.filter(function(l) {return !(colScale.domain().indexOf(l) > -1);});
+							vals.forEach(function(d) {
+								colScale.domain().push(d);
+								colScale.domain(colScale.domain()); // hack to register changes
+								colScale.range().push(refColors[curColInd]);
+								colScale.range(colScale.range());
+								curColInd = (curColInd + 1) % refColors.length;
+							});
+						} else {
+							for(var key in addedLayer.mapping.colors) {
+								colScale.domain().push(key);
+								colScale.domain(colScale.domain());
+								colScale.range().push(addedLayer.mapping.colors[key]);
+								colScale.range(colScale.range());
+							}
 						}
+
 					}
 
 					// adapt reference scales
@@ -318,29 +341,31 @@ angular.module('myApp.directives', ['myApp.filters']).
 					d3elmt.attr("height", height + margin.top + margin.bottom);
 					context.attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
 
-					yScale.domain().push({id: layer._id,
-																str: layer.label,
-																toString: function() {
-																	return this.str;
-																}});
-					yScale.domain(yScale.domain());
+					yScale.domain(scope.model.layers.map(function(el) {
+						return {id: el._id,
+							str: el.label,
+							toString: function() {
+								return this.str;
+							}}}));
 					yScale.rangeBands([0, height]);
 
 					focus.select(".y.axis").call(yAxis);
 					focus.select(".x.axis").call(xAxis);
 					context.select(".x.axis").call(xAxis2);
 
-					context.selectAll(".layer")
+					var layerSel = context.selectAll(".layer")
 						.data(scope.model.layers, function(d) {
 							return d._id;
-						})
-						.enter()
+						});
+
+					layerSel.enter()
 						.insert("g", ".brush")// insert before ".brush"
 						.attr("class", "layer")
 						.selectAll(".annot")
-						.data(layer.layer, function(d) {
-							return d._id;
+						.data(function(d) {
+							return d.layer;
 						})
+						// derive simple example to understand clearly why it does not work (example.html)
 						.enter()
 						.append("rect")
 						.attr("fill", "#999999")
@@ -355,31 +380,36 @@ angular.module('myApp.directives', ['myApp.filters']).
 						.attr("height", laneHeight)
 						.attr("class", "annot");
 
+					layerSel.exit().remove();
 
-
-					focus.selectAll(".layer")
+					layerSel = focus.selectAll(".layer")
 						.data(scope.model.layers, function(d) {
 							return d._id;
-						})
-						.enter()
+						});
+
+					layerSel.enter()
 						.append("g")
-						.attr("class", "layer")
-						.selectAll(".annot")
-						.data(layer.layer, function(d) {
-							return d._id;
+						.attr("class", "layer").selectAll(".annot")
+						.data(function(d) {
+							return d.layer;
 						})
 						.enter()
 						.append("rect")
 						.attr("fill", function(d) {
-							return colScale(layer.mapping.getKey(d));
+							return colScale(addedLayer.mapping.getKey(d));
 						})
 						.attr("opacity", 0.4)
 						.attr("y", 0)
 						.attr("height", laneHeight)
-
+						.attr("x", function(d) {
+							return xMsScale(d.fragment.start);
+						})
+						.attr("width", function(d) {
+							return xMsScale(d.fragment.end)-xMsScale(d.fragment.start);
+						})
 						.attr("class", "annot")
 						.on("mouseover", function(d){
-							tooltip.text(layer.tooltipFunc(d)); return tooltip.style("visibility", "visible");
+							tooltip.text(addedLayer.tooltipFunc(d)); return tooltip.style("visibility", "visible");
 						})
 						.on("mousemove", function(){
 							return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");
@@ -388,167 +418,39 @@ angular.module('myApp.directives', ['myApp.filters']).
 							return tooltip.style("visibility", "hidden");
 						});
 
+					layerSel.exit().remove();
 
-					// update all lane positions
-					focus.selectAll(".layer")
-						.attr("transform", function(d,i) {
-							return "translate(0," + (lanePadding+(i*(lanePadding+laneHeight))) + ")";
-						});
-
-
-				};
-
-				var removeLayer = function(layer) {
-					// seek color mapping for unused colors
-					var vals = [];
-					scope.model.layers.forEach(function(l) {
-						vals = vals.concat(l.layer.map(function(d) {
-							return l.mapping.getKey(d);
-						}));
-					});
-					// reorder according to current range
-					vals = colScale.domain().filter(function(l) {return (vals.indexOf(l) > -1);});
-					colScale.domain(vals);
-					colScale.range(vals.map(colScale));
-
-					// adapt timescales
-					var theMax = 0;
-					var curMax;
-					var maxDate = parseDate("00:00:00.000");
-					scope.model.layers.forEach(function(l) {
-						curMax = d3.max(l.layer.map(function(d) { return d.fragment.end; }));
-						theMax = d3.max([theMax, curMax]);
-						curMax = d3.max(l.layer.map(function(d) { return parseDate(secToTime(d.fragment.end)); }));
-						maxDate = d3.max([maxDate, curMax]);
+					// update positions of new selection
+					layerSel.attr("transform", function(d,i) {
+						return "translate(0," + (lanePadding+(i*(lanePadding+laneHeight))) + ")";
 					});
 
-					// adapt brush to new scale
-					var brushExtent = [];
-					if(!brush.empty()) {
-						brushExtent = brush.extent();
-						if(brushExtent[1] > theMax) {
-							brushExtent[1] = theMax;
-						}
-						if(brushExtent[0] > theMax) {
-							brush.clear();
-							brush(context.select(".brush"));
-						}
-					}
-
-					x2MsScale.domain([0, theMax]);
-					x2TimeScale.domain([parseDate("00:00:00.000"), maxDate]);
-
-					if(!brush.empty()){
-						brush.extent(brushExtent);
-						brush(context.select(".brush"));
-						brush.event(context.select(".brush")); // force refresh in case of clamping
-					}
-
-					if(brush.empty()|| scope.model.layers.length === 0) {
-						xMsScale.domain(x2MsScale.domain());
-						xTimeScale.domain(x2TimeScale.domain());
-					}
-
-
-
-					// adapt component size
-					height = (lanePadding+laneHeight) * scope.model.layers.length;
-					margin2.top = margin.top+height+contextPadding;
-					d3elmt.attr("height", height + margin.top + margin.bottom);
-					context.attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
-
-					// remove from yscale using _id
-					yScale.domain(yScale.domain().filter(function(e) {
-						return e.id !== layer._id;
-					}));
-					yScale.rangeBands([0, height]);
-
-					focus.select(".y.axis").call(yAxis);
-					focus.select(".x.axis").call(xAxis);
-					context.select(".x.axis").call(xAxis2);
-
-
-					// remove annotations and lanes
-					context.selectAll(".layer")
-						.data(scope.model.layers, function(d) {
-							return d._id;
-						})
-						.exit()
-						.remove();
-
-					focus.selectAll(".layer")
-						.data(scope.model.layers, function(d) {
-							return d._id;
-						})
-						.exit()
-						.remove();
-
-					// update all lane positions
-					focus.selectAll(".layer")
-						.attr("transform", function(d,i) {
-							return "translate(0," + (lanePadding+(i*(lanePadding+laneHeight))) + ")";
-						});
-
-				};
-
-
-
-				var refresh = function() {
-					focus.selectAll(".annot")
-						.attr("x", function(d) {
-							return xMsScale(d.fragment.start);
-						})
-						.attr("width", function(d) {
-							return xMsScale(d.fragment.end)-xMsScale(d.fragment.start);
-						})
 				};
 
 
 
 
+				// instead, updating layer function
+				// updateLayers(newLayer, oldLayer)
 
-
-				scope.$watch('model.layers.length', function(newValue, oldValue) {
-					// add case : find layers that are in the new object, but not in old
-					// use addLayer and remove layer to handle heavy operations
-
+				scope.$watch('model.layerWatch', function(newValue, oldValue) {
 					// watches are executed at initialization, even if latestLayer is still undefined
 					// -> care with tests
-					var isAdded = (newValue - oldValue) === 1;
-					var isRemoved = (newValue - oldValue) === -1;
 
-					if(isAdded) {
-						addLayer(scope.model.latestLayer);
-					}
-					if(isRemoved) {
-						removeLayer(scope.model.latestLayer);
-					}
-					refresh();
+					var addedLayerId = newValue.filter(function(l) {
+						return !(oldValue.indexOf(l) > -1);
+					});
+					addedLayerId = (addedLayerId.length > 0) ? addedLayerId[0] : undefined;
 
 
-				});
+					var removedLayerId = oldValue.filter(function(l) {
+						return !(newValue.indexOf(l) > -1);
+					});
+					removedLayerId = (removedLayerId.length > 0) ? removedLayerId[0] : undefined;
 
+					updateLayers(addedLayerId, removedLayerId);
 
-// watchCollection still buggy (see https://github.com/angular/angular.js/issues/2621)
-// alternatively, watch layers length, and use latestLayer for quicker reference
-//				scope.$watchCollection('model.layers', function(newLayers, oldLayers, scope) {
-//					// add case : find layers that are in the new object, but not in old
-//					// use addLayer and remove layer to handle heavy operations
-//
-//					// watches are executed at initialization, even if latestLayer is still undefined
-//					// -> care with tests
-//					console.log("added");
-//					var isAdded = (scope.model.layers.indexOf(scope.model.latestLayer) > -1);
-//
-//					if(isAdded) {
-//						addLayer(scope.model.latestLayer);
-//					} else if(scope.model.latestLayer !== undefined) {
-//						removeLayer(scope.model.latestLayer);
-//					}
-//					refresh();
-//
-//
-//				});
+				}, true);
 
 
 				init();
