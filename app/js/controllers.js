8198d0108a613f04757903a9db60c73aed692ce1
Hervé BREDIN <bredin@limsi.fr>
2013-11-17 15:07:45 +0100

    Initial commit of demo for ERRARE workshop

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -94,26 +94,28 @@ angular.module('myApp.controllers', ['myApp.services'])
 		$scope.model.layers = [
 			{
 				'label': 'Reference',
-				'_id': 0,
+				'_id': null,
 				'layer': [],
 				'mapping': null,
 				'tooltipFunc': null
 			},
 			{
 				'label': 'Hypothesis',
-				'_id': 1,
+				'_id': null,
 				'layer': [],
 				'mapping': null,
 				'tooltipFunc': null
 			},
 			{
 				'label': 'Difference',
-				'_id': 2,
+				'_id': null,
 				'layer': [],
 				'mapping': null,
 				'tooltipFunc': null
 			}
 		];
+
+		$scope.model.layerWatch = [null, null, null];
 
 		// selected corpus ID
 		$scope.model.selected_corpus = "";
@@ -158,10 +160,17 @@ angular.module('myApp.controllers', ['myApp.services'])
 						'label': 'Reference',
 						'_id': layer_id,
 						'layer': $scope.model.reference,
-						'mapping': null,
-     				'tooltipFunc': null
+						'mapping': {
+							'getKey': function(d) {
+								return d.data;
+							}
+						},
+     					'tooltipFunc': function(d) {
+     						return d.data;
+     					}
 					}
 					$scope.model.layers[0] = layer;
+					$scope.model.layerWatch[0] = layer_id;
 					$scope.model.latestLayer = layer;
 					$scope.compute_diff();
 				}
@@ -178,10 +187,17 @@ angular.module('myApp.controllers', ['myApp.services'])
 						'label': 'Hypothesis',
 						'_id': layer_id,
 						'layer': $scope.model.hypothesis,
-						'mapping': null,
-     				'tooltipFunc': null
+						'mapping': {
+							'getKey': function(d) {
+								return d.data;
+							}
+						},
+     					'tooltipFunc': function(d) {
+     						return d.data;
+     					}
 					}
 					$scope.model.layers[1] = layer;
+					$scope.model.layerWatch[1] = layer_id;
 					$scope.model.latestLayer = layer;
 					$scope.compute_diff();
 				});
@@ -197,14 +213,28 @@ angular.module('myApp.controllers', ['myApp.services'])
 
 			CMError.diff(reference_and_hypothesis).success(function(data, status) {
 				$scope.model.diff = data;
+				var layer_id = $scope.model.layers[0]._id + '_vs_' + $scope.model.layers[1]._id;
 				var layer = {
 					'label': 'Difference',
-					'_id': $scope.model.layers[0]._id + '_vs_' + $scope.model.layers[1]._id,
+					'_id': layer_id,
 					'layer': $scope.model.diff,
-					'mapping': null,
- 					'tooltipFunc': null
+					'mapping': {
+						'colors': {
+							"correct": "green",
+							"miss": "orange",
+							"false alarm": "orange",
+							"confusion": "red"
+						},
+						'getKey': function(d) {
+							return d.data[0];
+						}
+					},
+  					'tooltipFunc': function(d) {
+     						return d.data[0];
+   					}
 				}
 				$scope.model.layers[2] = layer;
+				$scope.model.layerWatch[2] = layer_id;
 				$scope.model.latestLayer = layer;
 			});
 		}
@@ -248,11 +278,61 @@ angular.module('myApp.controllers', ['myApp.services'])
 	  delete $http.defaults.headers.common['X-Requested-With'];
 
 		$scope.model = {};
+
+		// layers[0] is the reference
+		// layers[1] is the first hypothesis
+		// layers[2] is the second hypothesis
+		// layers[3] is the regression layer
+
+		$scope.model.layers = [
+			{
+				'label': 'Reference',
+				'_id': null,
+				'layer': [],
+				'mapping': null,
+				'tooltipFunc': null
+			},
+			{
+				'label': 'Hypothesis 1',
+				'_id': null,
+				'layer': [],
+				'mapping': null,
+				'tooltipFunc': null
+			},
+			{
+				'label': 'Hypothesis 2',
+				'_id': null,
+				'layer': [],
+				'mapping': null,
+				'tooltipFunc': null
+			},
+			{
+				'label': 'Regression',
+				'_id': null,
+				'layer': [],
+				'mapping': null,
+				'tooltipFunc': null
+			}
+		];
+
+		$scope.model.layerWatch = [null, null, null, null];
+
+		// selected corpus ID
 		$scope.model.selected_corpus = "";
+
+		// selected medium ID
 		$scope.model.selected_medium = "";
+
+		// selected reference layer ID
 		$scope.model.selected_reference = "";
+
+		// selected first hypothesis layer ID
 		$scope.model.selected_before = "";
+
+		// selected second hypothesis layer ID
 		$scope.model.selected_after = "";
+
+		// list of annotations
 		$scope.model.reference = [];
 		$scope.model.before = [];
 		$scope.model.after = [];
@@ -274,19 +354,76 @@ angular.module('myApp.controllers', ['myApp.services'])
 		$scope.get_reference_annotations = function(corpus_id, medium_id, layer_id) {
 			$scope.model.reference = Annotation.query(
 				{corpusId: corpus_id, mediaId: medium_id, layerId: layer_id},
-				$scope.compute_regression);
+				function() {
+					var layer = {
+						'label': 'Reference',
+						'_id': layer_id,
+						'layer': $scope.model.reference,
+						'mapping': {
+							'getKey': function(d) {
+								return d.data;
+							}
+						},
+     					'tooltipFunc': function(d) {
+     						return d.data;
+     					}
+					}
+					$scope.model.layers[0] = layer;
+					$scope.model.layerWatch[0] = layer_id;
+					$scope.model.latestLayer = layer;
+					$scope.compute_regression();
+				}
+			);
 		};
 
 		$scope.get_before_annotations = function(corpus_id, medium_id, layer_id) {
 			$scope.model.before = Annotation.query(
 				{corpusId: corpus_id, mediaId: medium_id, layerId: layer_id},
-				$scope.compute_regression);
+				function() {
+					var layer = {
+						'label': 'Hypothesis 1',
+						'_id': layer_id,
+						'layer': $scope.model.before,
+						'mapping': {
+							'getKey': function(d) {
+								return d.data;
+							}
+						},
+     					'tooltipFunc': function(d) {
+     						return d.data;
+     					}
+					}
+					$scope.model.layers[1] = layer;
+					$scope.model.layerWatch[1] = layer_id;
+					$scope.model.latestLayer = layer;
+					$scope.compute_regression();
+				}
+			);
 		};
 
 		$scope.get_after_annotations = function(corpus_id, medium_id, layer_id) {
 			$scope.model.after = Annotation.query(
 				{corpusId: corpus_id, mediaId: medium_id, layerId: layer_id},
-				$scope.compute_regression);
+				function() {
+					var layer = {
+						'label': 'Hypothesis 2',
+						'_id': layer_id,
+						'layer': $scope.model.after,
+						'mapping': {
+							'getKey': function(d) {
+								return d.data;
+							}
+						},
+     					'tooltipFunc': function(d) {
+     						return d.data;
+     					}
+					}
+					$scope.model.layers[2] = layer;
+					$scope.model.layerWatch[2] = layer_id;
+					$scope.model.latestLayer = layer;
+					$scope.compute_regression();
+				}
+			);
 		};
 
 		$scope.compute_regression = function() {
@@ -299,6 +436,29 @@ angular.module('myApp.controllers', ['myApp.services'])
 
 			CMError.regression(reference_and_hypotheses).success(function(data, status) {
 				$scope.model.regression = data;
+				var layer_id = $scope.model.layers[1]._id + '_vs_' + $scope.model.layers[2]._id;
+				var layer = {
+					'label': 'Regression',
+					'_id': layer_id,
+					'layer': $scope.model.regression,
+					'mapping': {
+						'colors': {
+							"both_correct": "white",
+							"both_incorrect": "black",
+							"improvement": "green",
+							"regression": "red"
+						},
+						'getKey': function(d) {
+							return d.data[0];
+						}
+					},
+ 					'tooltipFunc': function(d) {
+ 						return d.data[0];
+ 					}
+				}
+				$scope.model.layers[3] = layer;
+				$scope.model.layerWatch[3] = layer_id;
+				$scope.model.latestLayer = layer;
 			});
 		}
 
