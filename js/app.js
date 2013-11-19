953f5b901083b215ea547713cf8e6604730001d7
Hervé BREDIN <bredin@limsi.fr>
2013-11-19 11:35:49 +0100

    Routes /diff and /regression are functional

--- js/app.js
+++ js/app.js
@@ -5,9 +5,9 @@ angular.module(
     'myApp',
     ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers', 'ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
-    // $routeProvider.when('/analysis', {templateUrl: 'partials/analysis.html'});
     $routeProvider.when('/diff', {templateUrl: 'partials/diff.html'});
     $routeProvider.when('/regression', {templateUrl: 'partials/regression.html'});
-    $routeProvider.when('/errare', {templateUrl: 'partials/errare.html'});
+    $routeProvider.when('/fusion', {templateUrl: 'partials/fusion.html'});
+    // $routeProvider.when('/errare', {templateUrl: 'partials/errare.html'});
     $routeProvider.otherwise({redirectTo: '/'});
 }]);
