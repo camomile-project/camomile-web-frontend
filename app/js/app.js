4bcab26ab861690b45b89e4599e3187e8c9fe3d9
Pierrick Bruneau <bruneau@lippmann.lu>
2014-06-24 10:31:00 +0200

    feat/rightclk: unstable

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,13 @@
 // Declare app level module which depends on filters, and services
 angular.module(
     'myApp',
-    ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers', 'ngRoute', 'ngCookies'])
+    [	'ngRoute',
+			'ngCookies',
+			'mgcrea.ngStrap',
+			'myApp.filters',
+			'myApp.services',
+			'myApp.directives',
+			'myApp.controllers'])
 .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/diff', {templateUrl: 'partials/diff.html'});
     $routeProvider.when('/regression', {templateUrl: 'partials/regression.html'});
