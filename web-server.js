2f743394ddaaf13fa96c98f0d04e5e09cee61113
Herve Bredin <bredin@limsi.fr>
2014-10-20 16:25:13 +0200

    feat: add --camomile and --pyannote command line parameters

--- web-server.js
+++ web-server.js
@@ -1,6 +1,10 @@
 var express = require("express"),
 	app     = express(),
-	port    = parseInt(process.env.PORT, 10) || 8070;
+	port    = parseInt(process.env.PORT, 10) || 8070,
+    program = require('commander'),
+    fs = require('fs');
+
+sprintf = require('sprintf').sprintf;
 
 app.configure(function(){
 	app.use(express.methodOverride());
@@ -9,9 +13,44 @@ app.configure(function(){
 	app.use(app.router);
 });
 
+
+// === config.js === 
+// angular.module('myApp.config', [])
+//     .value('DataRoot', 'https://flower.limsi.fr/api')
+//     .value('ToolRoot', 'https://flower.limsi.fr/tool');
+// =================
+
+program
+    .option('-c, --camomile <url>', 'base URL of Camomile server (e.g. https://camomile.fr/api)')
+    .option('-p, --pyannote <url>', 'base URL of PyAnnote server (e.g. https://camomile.fr/tool)')
+    .parse(process.argv);
+
+var camomile_api = program.camomile || process.env.CAMOMILE_API
+var pyannote_api = program.pyannote || process.env.PYANNOTE_API
+
+config_js = sprintf(
+    "angular.module('myApp.config', [])" + "\n" + 
+    "   .value('DataRoot', '%s')" + "\n" +
+    "   .value('ToolRoot', '%s');",
+    camomile_api, pyannote_api
+)
+
+fs.writeFile(
+    'config.js', config_js, 
+    function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log('Camomile API --> ' + camomile_api);
+            console.log('PyAnnote API --> ' + pyannote_api);
+        }
+    }
+); 
+
 app.post('/', function(req, res){
 	res.redirect('/'); // handle the hidden form submit
 });
 
 app.listen(port);
-console.log('Now serving the app at http://localhost:' + port + '/');
+console.log('Web App --> ' + 'http://localhost:' + port + '/');
+
