8a949104e17c97371bbded8de2c9c6c9b85b2dbe
unknown <isc@ws-01795.crpgl.lu>
2014-10-21 16:40:24 +0200

    Now only display pages when logged

--- app/config.js
+++ app/config.js
@@ -1,3 +1,3 @@
 angular.module('myApp.config', [])
-   .value('DataRoot', 'https://flower.limsi.fr/crpgl/api')
+   .value('DataRoot', 'http://localhost:3000')
    .value('ToolRoot', 'https://flower.limsi.fr/tool');
