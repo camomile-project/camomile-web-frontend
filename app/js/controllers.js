6be9fd839c680e0be8b571df0501fa90e07c3002
Mickaël Stefas <stefas@lippmann.lu>
2015-02-18 14:17:20 +0100

    Remove the call of addFakeValues()

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1943,7 +1943,7 @@ angular.module('myApp.controllers', ['myApp.services'])
             // TODO: This have to be uncommented only for tests. it creates queues on the server. Also, latest server version do it its own way, so not necessary
             //	          $scope.model.createFakeQueue();
             //TODO:  This have to be uncommented only for tests. It add fake values in queues stored server side. Will be removed when all will be ok.
-            $scope.model.addFakeValues();
+//            $scope.model.addFakeValues();
 
             // reset all queues
             //    db.queues.update({},{ $set: { queue: [] } }, {multi:true})
