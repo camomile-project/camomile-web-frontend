9da7ec01d22c4ea042001aeb289f3f26b27e6596
xiuming xu <xu@m60.(none)>
2015-07-02 15:56:38 +0200

    add keystroke and higher opacity of controlbar(css)

--- app/js/controllers/labelCtrl.js
+++ app/js/controllers/labelCtrl.js
@@ -2,9 +2,9 @@
  * Created by isc on 12/05/15.
  */
 angular.module('myApp.controllers')
-    .controller('labelCtrl', ['$sce', '$scope', '$http',
+    .controller('labelCtrl', ['$document', '$sce', '$scope', '$http',
         'defaults', '$controller', 'Session', '$rootScope', 'camomileService',
-        function ($sce, $scope, $http, defaults, $controller, Session, $rootScope, camomileService) {
+        function ($document, $sce, $scope, $http, defaults, $controller, Session, $rootScope, camomileService) {
 
             $controller('CommonCtrl', {
                 $scope: $scope,
@@ -353,6 +353,82 @@ angular.module('myApp.controllers')
                         $scope.model.popQueueElement();
                     })
             };
+				$document.on(
+					"keydown",
+					function (event) {
+						var targetID = event.target.id;
+						var button_checked = false;
+						if (targetID == 'confirm' || targetID == 'cancel') {
+							button_checked = true;
+						}
+						//enter
+						if (event.keyCode == 13 && targetID != 'localServerInput') {
+							//If the focus is on the check buttons, blur the focus first
+							if (button_checked) {
+								event.target.blur();
+							}
+							$scope.$apply(function () {
+								$scope.model.saveQueueElement(true);
+							});
+						}
+						//space
+						if (event.keyCode == 32 && targetID != "entry_input") {
+							if (button_checked) {
+								event.target.blur();
+							}
+							$scope.$apply(function () {
+								$scope.model.toggle_play();
+							});
+						}
+						//esc-> skip
+						if (event.keyCode == 27) {
+							$scope.$apply(function () {
+								//skip
+								$scope.model.saveQueueElement(false);
+							});
+						}
+						//Left
+						if (event.keyCode == 37) {
+							$scope.$apply(function () {
+								if ($scope.model.current_time - 0.04 > $scope.model.infbndsec) {
+									$scope.model.current_time = $scope.model.current_time - 0.04;
+								} else {
+									$scope.model.current_time = $scope.model.infbndsec;
+								}
+							});
+						}
+						//Right
+						if (event.keyCode == 39) {
+							$scope.$apply(function () {
+								if ($scope.model.current_time + 0.04 < $scope.model.supbndsec) {
+									$scope.model.current_time = $scope.model.current_time + 0.04;
+								} else {
+									$scope.model.current_time = $scope.model.supbndsec;
+								}
+							});
+						}
+						//Up
+						if (event.keyCode == 38) {
+							$scope.$apply(function () {
+								if ($scope.model.current_time - 1 > $scope.model.infbndsec) {
+									$scope.model.current_time = $scope.model.current_time - 1;
+								} else {
+									$scope.model.current_time = $scope.model.infbndsec;
+								}
+							});
 
+						} //Down
+						if (event.keyCode == 40) {
+							$scope.$apply(function () {
+
+								if ($scope.model.current_time + 1 < $scope.model.supbndsec) {
+									$scope.model.current_time = $scope.model.current_time + 1;
+								} else {
+									$scope.model.current_time = $scope.model.supbndsec;
+								}
+							});
+						}
+					}
+				);
         }
     ]);
