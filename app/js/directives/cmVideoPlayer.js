fcc94a8d1e8ca4fa2d905f3e174b7a0bf18e9176
xiuming xu <xu@m60.(none)>
2015-07-02 15:57:53 +0200

    make video autoloop

--- app/js/directives/cmVideoPlayer.js
+++ app/js/directives/cmVideoPlayer.js
@@ -10,7 +10,10 @@ angular.module('myApp.directives')
 				scope.model.current_time = 0;
 				scope.model.restrict_toggle = 0;
 				scope.model.infbndsec = 0;
-
+				scope.model.loop = true;
+				scope.model.loop_display = function() {
+                  			scope.model.loop = !scope.model.loop;
+                		};
 
 				scope.model.toggle_play = function (value) {
 					if (scope.model.play_state !== undefined) {
@@ -67,14 +70,19 @@ angular.module('myApp.directives')
 
 				element[0].addEventListener("timeupdate", function () {
 					scope.$apply(function () {
-						// if player paused, currentTime has been changed for exogenous reasons
+						// if player paused,  has been changed for exogenous reasons
 						if (!element[0].paused) {
 							if (element[0].currentTime > scope.model.supbndsec) {
-								scope.model.toggle_play(false);
-								scope.model.current_time = scope.model.supbndsec;
-							} else {
-								scope.model.current_time = element[0].currentTime;
-							}
+                               					if (scope.model.loop === false) {
+                                    					scope.model.toggle_play(false);
+                                    					scope.model.current_time = scope.model.supbndsec;
+                                				}else {
+                                    					scope.model.toggle_play(true);
+                                    					scope.model.current_time = scope.model.infbndsec;
+                               				}
+                            			} else {
+                                			scope.model.current_time = element[0].currentTime;
+                            				}	
 						}
 					});
 				});
